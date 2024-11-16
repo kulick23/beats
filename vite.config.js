4332150e7abac5308a68c49512148f3d04dc41b3
Danila Kulinkovich <danek200326@gmail.com>
2024-11-16 06:43:04 +0200

    fix(ESDU-00): fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/beats/', 
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
 });
